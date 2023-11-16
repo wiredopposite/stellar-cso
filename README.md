1fecfdd6e8d3613c83994694b90d78289de01e7c
wiredopposite <44521229+wiredopposite@users.noreply.github.com>
2023-11-16 13:02:00 -0700

    Added split ISO input
    Added split ISO input

--- README.md
+++ README.md
@@ -9,24 +9,24 @@
 I'll be using this repo to add features to the ciso script and flesh it out a bit more.
 
 ## Added features
-- The ability to provide a directory path as an input argument, the script will process all .ISO files within that directory.
-- The ability to scrub ISO files, zeroes out padding sectors and makes compression of Redump images more efficient.
+- Ability to provide a directory path as an input argument, the script will process all .ISO files within that directory, including split ISOs.
+- Ability to scrub ISO files, zeroes out padding sectors and makes compression of Redump images more efficient.
+- Ability to process ISO files that are already split into files ending in '.1.iso' and '.2.iso'. The script will find the other part of the ISO file if either is provided as an input argument, assuming they both have the same name.
 
 ## Usage
 ```bash
-python3 ciso.py <ISO/XISO Path>
+python3 ciso.py <ISO/XISO or Folder Path>
 ```
 To enable ISO scrubbing/trimming:
 ```bash
-python3 ciso.py -s <ISO/XISO Path>
+python3 ciso.py -s <ISO/XISO or Folder Path>
 ```
 or
 ```bash
-python3 ciso.py --scrub <ISO/XISO Path>
+python3 ciso.py --scrub <ISO/XISO or Folder Path>
 ```
 
 ## About
-
 Compression script is based on, and forked, from [https://github.com/phyber/ciso](https://github.com/phyber/ciso) under the BSD-3-Clause license.
 
 Based on ciso from [https://github.com/jamie/ciso](https://github.com/jamie/ciso).
--- ciso.py
+++ ciso.py
@@ -27,6 +27,69 @@ class BinaryTreeNode:
         self.DirectoryPos = DirectoryPos
         self.Offset = Offset
 
+class SplitInput:
+    def __init__(self, file1_path, file2_path):
+        self.file1 = open(file1_path, 'rb')
+        self.file2 = open(file2_path, 'rb')
+        self.current_file = self.file1
+
+    def read(self, size=-1):
+        data = self.current_file.read(size)
+        if size != -1:
+            remaining_size = size - len(data)
+            # if still more requested data after the end of first file
+            if remaining_size > 0 and self.current_file is self.file1:
+                self.current_file = self.file2
+                data += self.current_file.read(remaining_size)
+        else:
+            if self.current_file is self.file1:
+                self.current_file = self.file2
+                data += self.current_file.read()
+
+        return data
+
+    def seek(self, offset, whence=os.SEEK_SET):
+        file1_size = os.fstat(self.file1.fileno()).st_size
+
+        if whence == os.SEEK_SET:
+            if offset < file1_size:
+                self.file1.seek(offset)
+                self.current_file = self.file1
+                self.file2.seek(0) 
+            else:
+                self.file1.seek(file1_size)  # go to end of file1
+                self.file2.seek(offset - file1_size)
+                self.current_file = self.file2
+        elif whence == os.SEEK_CUR:
+            current_pos = self.file1.tell() if self.current_file is self.file1 else file1_size + self.file2.tell()
+            self.seek(current_pos + offset)
+        elif whence == os.SEEK_END:
+            file2_size = os.fstat(self.file2.fileno()).st_size
+            total_size = file1_size + file2_size
+            self.seek(total_size + offset)
+        else:
+            raise ValueError("Invalid value for `whence`.")
+
+    def tell(self):
+        file1_pos = self.file1.tell()
+        file2_pos = self.file2.tell()
+        
+        if self.current_file is self.file1:
+            return file1_pos
+        else:
+            file1_size = os.fstat(self.file1.fileno()).st_size
+            return file1_size + file2_pos
+
+    def close(self):
+        self.file1.close()
+        self.file2.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
 def get_terminal_size(fd=sys.stdout.fileno()):
 	try:
 		import fcntl, termios
@@ -241,141 +304,138 @@ def get_security_sectors(f, data_sectors, sector_offset):
 	print()
 	return security_sectors
 
-def compress_iso(infile, scrub):
+def compress_iso(fin, infile, scrub):
 	lz4_context = lz4.frame.create_compression_context()
 
 	# Replace file extension with .cso
 	fout_1 = open(os.path.splitext(infile)[0] + '.1.cso', 'wb')
 	fout_2 = None
 
-	with open(infile, 'rb') as fin:
-		print("Compressing '{}'".format(infile))
+	# Detect and validate the ISO
+	detect_iso_type(fin)
 
-		# Detect and validate the ISO
-		detect_iso_type(fin)
+	ciso, total_infile_sectors = check_file_size(fin)
 
-		ciso, total_infile_sectors = check_file_size(fin)
+	if scrub:
+		sector_offset = image_offset // XISO_SECTOR_SIZE
 
-		if scrub:
-			sector_offset = image_offset // XISO_SECTOR_SIZE
+		# Get sectors from infile's directory table
+		data_sectors = get_data_sectors(fin, sector_offset)
 
-			# Get sectors from infile's directory table
-			data_sectors = get_data_sectors(fin, sector_offset)
+		# Trim to last data sector + 1 if that comes before last infile sector
+		ciso['total_blocks'] = min(max(data_sectors) + 1, total_infile_sectors) - sector_offset
+		ciso['total_bytes']  = ciso['total_blocks'] * XISO_SECTOR_SIZE
 
-			# Trim to last data sector + 1 if that comes before last infile sector
-			ciso['total_blocks'] = min(max(data_sectors) + 1, total_infile_sectors) - sector_offset
-			ciso['total_bytes']  = ciso['total_blocks'] * XISO_SECTOR_SIZE
+		# We don't need security sectors if input isn't a redump
+		# 0x374750 is the # of sectors in a redump image minus video partition
+		if total_infile_sectors - sector_offset == 0x374750:
+			security_sectors = list(get_security_sectors(fin, data_sectors, sector_offset))
+			data_sectors.update(security_sectors)
 
-			# We don't need security sectors if input isn't a redump
-			# 0x374750 is the # of sectors in a redump image minus video partition
-			if total_infile_sectors - sector_offset == 0x374750:
-				security_sectors = list(get_security_sectors(fin, data_sectors, sector_offset))
-				data_sectors.update(security_sectors)
+		# Shift data sectors to account for image offset
+		data_sectors = {x - sector_offset for x in data_sectors}
 
-			# Shift data sectors to account for image offset
-			data_sectors = {x - sector_offset for x in data_sectors}
+	for k, v in ciso.items():
+		print("{}: {}".format(k, v))
 
-		for k, v in ciso.items():
-			print("{}: {}".format(k, v))
+	write_cso_header(fout_1, ciso)
+	block_index = [0x00] * (ciso['total_blocks'] + 1)
 
-		write_cso_header(fout_1, ciso)
-		block_index = [0x00] * (ciso['total_blocks'] + 1)
+	# Write the dummy block index for now.
+	write_block_index(fout_1, block_index)
 
-		# Write the dummy block index for now.
-		write_block_index(fout_1, block_index)
+	write_pos = fout_1.tell()
+	align_b = 1 << ciso['align']
+	align_m = align_b - 1
 
-		write_pos = fout_1.tell()
-		align_b = 1 << ciso['align']
-		align_m = align_b - 1
+	# Alignment buffer is unsigned char.
+	alignment_buffer = struct.pack('<B', 0x00) * 64
 
-		# Alignment buffer is unsigned char.
-		alignment_buffer = struct.pack('<B', 0x00) * 64
+	# Progress counters
+	percent_period = ciso['total_blocks'] / 100
+	percent_cnt = 0
 
-		# Progress counters
-		percent_period = ciso['total_blocks'] / 100
-		percent_cnt = 0
+	split_fout = fout_1
 
-		split_fout = fout_1
+	fin.seek(image_offset, os.SEEK_SET)
 
-		fin.seek(image_offset, os.SEEK_SET)
+	for block in range(0, ciso['total_blocks']):
+		# Check if we need to split the ISO (due to FATX limitations)
+		# TODO: Determine a better value for this.
+		if write_pos > 0xFFBF6000:
+			# Create new file for the split
+			fout_2     = open(os.path.splitext(infile)[0] + '.2.cso', 'wb')
+			split_fout = fout_2
 
-		for block in range(0, ciso['total_blocks']):
-			# Check if we need to split the ISO (due to FATX limitations)
-			# TODO: Determine a better value for this.
-			if write_pos > 0xFFBF6000:
-				# Create new file for the split
-				fout_2     = open(os.path.splitext(infile)[0] + '.2.cso', 'wb')
-				split_fout = fout_2
+			# Reset write position
+			write_pos  = 0
 
-				# Reset write position
-				write_pos  = 0
+		# Write alignment
+		align = int(write_pos & align_m)
+		if align:
+			align = align_b - align
+			size = split_fout.write(alignment_buffer[:align])
+			write_pos += align
 
-			# Write alignment
-			align = int(write_pos & align_m)
-			if align:
-				align = align_b - align
-				size = split_fout.write(alignment_buffer[:align])
-				write_pos += align
+		# Mark offset index
+		block_index[block] = write_pos >> ciso['align']
 
-			# Mark offset index
-			block_index[block] = write_pos >> ciso['align']
+		if scrub and block not in data_sectors:
+			# Zero out padding
+			raw_data = bytes([0] * ciso['block_size'])
+			fin.seek(ciso['block_size'], os.SEEK_CUR)
+		else:
+			# Read raw data
+			raw_data = fin.read(ciso['block_size'])
 
-			if scrub and block not in data_sectors:
-				# Zero out padding
-				raw_data = bytes([0] * ciso['block_size'])
-				fin.seek(ciso['block_size'], os.SEEK_CUR)
-			else:
-				# Read raw data
-				raw_data = fin.read(ciso['block_size'])
+		raw_data_size = len(raw_data)
 
-			raw_data_size = len(raw_data)
+		# Compress block
+		# Compressed data will have the gzip header on it, we strip that.
+		lz4.frame.compress_begin(lz4_context, compression_level=lz4.frame.COMPRESSIONLEVEL_MAX,
+			auto_flush=True, content_checksum=False, block_checksum=False, block_linked=False, source_size=False)
 
-			# Compress block
-			# Compressed data will have the gzip header on it, we strip that.
-			lz4.frame.compress_begin(lz4_context, compression_level=lz4.frame.COMPRESSIONLEVEL_MAX,
-				auto_flush=True, content_checksum=False, block_checksum=False, block_linked=False, source_size=False)
+		compressed_data = lz4.frame.compress_chunk(lz4_context, raw_data, return_bytearray=True)
+		compressed_size = len(compressed_data)
 
-			compressed_data = lz4.frame.compress_chunk(lz4_context, raw_data, return_bytearray=True)
-			compressed_size = len(compressed_data)
+		lz4.frame.compress_flush(lz4_context)
 
-			lz4.frame.compress_flush(lz4_context)
+		# Ensure compressed data is smaller than raw data
+		# TODO: Find optimal block size to avoid fragmentation
+		if (compressed_size + 12) >= raw_data_size:
+			writable_data = raw_data
 
-			# Ensure compressed data is smaller than raw data
-			# TODO: Find optimal block size to avoid fragmentation
-			if (compressed_size + 12) >= raw_data_size:
-				writable_data = raw_data
+			# Next index
+			write_pos += raw_data_size
+		else:
+			writable_data = compressed_data
 
-				# Next index
-				write_pos += raw_data_size
-			else:
-				writable_data = compressed_data
+			# LZ4 block marker
+			block_index[block] |= 0x80000000
 
-				# LZ4 block marker
-				block_index[block] |= 0x80000000
+			# Next index
+			write_pos += compressed_size
 
-				# Next index
-				write_pos += compressed_size
+		# Write data
+		split_fout.write(writable_data)
 
-			# Write data
-			split_fout.write(writable_data)
+		# Progress bar
+		percent = int(round((block / (ciso['total_blocks'] + 1)) * 100))
+		if percent > percent_cnt:
+			update_progress((block / (ciso['total_blocks'] + 1)))
+			percent_cnt = percent
 
-			# Progress bar
-			percent = int(round((block / (ciso['total_blocks'] + 1)) * 100))
-			if percent > percent_cnt:
-				update_progress((block / (ciso['total_blocks'] + 1)))
-				percent_cnt = percent
+	# TODO: Pad file to ATA block size
 
-		# TODO: Pad file to ATA block size
+	# end for block
+	# last position (total size)
+	# NOTE: We don't actually need this, but we're keeping it for legacy reasons.
+	block_index[-1] = write_pos >> ciso['align']
 
-		# end for block
-		# last position (total size)
-		# NOTE: We don't actually need this, but we're keeping it for legacy reasons.
-		block_index[-1] = write_pos >> ciso['align']
-
-		# write header and index block
-		print("\nWriting block index")
-		fout_1.seek(CISO_HEADER_SIZE, os.SEEK_SET)
-		write_block_index(fout_1, block_index)
+	# write header and index block
+	print("\nWriting block index")
+	fout_1.seek(CISO_HEADER_SIZE, os.SEEK_SET)
+	write_block_index(fout_1, block_index)
 
 	# end open(infile)
 	pad_file_size(fout_1)
@@ -384,6 +444,43 @@ def compress_iso(infile, scrub):
 	if fout_2:
 		pad_file_size(fout_2)
 		fout_2.close()
+
+def get_paired_file(infile, extension):
+    paired_file = infile[:-6] + extension
+    if not os.path.exists(paired_file):
+        return None
+    return paired_file
+
+def open_infile(infile):
+	infile_no_ext, extension = os.path.splitext(infile)
+	subextension = None
+
+	if infile_no_ext.endswith('.1') or infile_no_ext.endswith('.2'):
+		_, subextension = os.path.splitext(infile_no_ext)
+
+		# Find split file parts and combine into a single file object, readable with the SplitInput class
+		if subextension == '.1':
+			infile2 = get_paired_file(infile, ('.2' + extension))
+			if not infile2:
+				# subextension .1 doesn't always mean it's a split file, continue anyway
+				print(f"File has subextension '.1' but part 2 was not found, proceeding anyway.\nProcessing: '{infile}'")
+				f = open(infile, 'rb')
+			else:
+				print(f"Processing split '{extension}' file.\nPart 1: '{infile}'\nPart 2: '{infile2}'")
+				f = SplitInput(infile, infile2)
+		elif subextension == '.2':
+			infile1 = get_paired_file(infile, ('.1' + extension))
+			if not infile1:
+				print(f'Paired file for {os.path.basename(infile)} not found.')
+				return False
+			
+			print(f"Processing split '{extension}' file.\nPart 1: '{infile1}'\nPart 2: '{infile}'")
+			f = SplitInput(infile1, infile)
+	else:
+		print(f"Processing: '{infile}'")
+		f = open(infile, 'rb')
+
+	return f
 
 def main(argv):
 	input_path = None
@@ -399,13 +496,24 @@ def main(argv):
 		print("Error: You must specify an input file.")
 		sys.exit(1)
 	elif os.path.isfile(input_path):
-		compress_iso(input_path, scrub)
+		infile = input_path
+		fin = open_infile(infile)
+		if fin:
+			compress_iso(fin, infile, scrub)
+			fin.close()
+		else:
+			sys.exit(1)
 	elif os.path.isdir(input_path):
 		for root, folders, files in os.walk(input_path):
 			for file in files:
-				if file.endswith('.iso'):
+				if file.endswith('.iso') and not file.endswith('.2.iso'):
 					infile = os.path.join(root, file)
-					compress_iso(infile, scrub)
+					fin = open_infile(infile)
+					if fin:
+						compress_iso(fin, infile, scrub)
+						fin.close()
+					else:
+						print(f"Skipping '{infile}'")
 
 if __name__ == '__main__':
 	sys.exit(main(sys.argv))
